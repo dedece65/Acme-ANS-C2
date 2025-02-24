d98aa839a964c96f32da6c5c09b740530823eda1
Enrgaraba <enrgaraba@alum.us.es>
2025-02-24 14:03:57 +0100

    Student2: Primera version Customer. #18

--- src/main/java/acme/entities/customer/Customer.java
+++ src/main/java/acme/entities/customer/Customer.java
@@ -1,9 +1,18 @@
 
 package acme.entities.customer;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.constraints.ValidIdentifier;
+import acme.constraints.ValidLongText;
+import acme.constraints.ValidShortText;
+import acme.datatypes.Phone;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,8 +23,37 @@ public class Customer extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
 
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidIdentifier
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@Automapped
+	private Phone				phoneNumber;
+
+	@Mandatory
+	@ValidLongText
+	@Automapped
+	private String				address;
+
+	@Mandatory
+	@ValidShortText
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidShortText
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidNumber(min = 0, max = 500000)
+	@Automapped
+	private int					earnedPoints;
 
 }
