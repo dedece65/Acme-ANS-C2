e96440d8374b95c7819e5d5fb1f904d260b31540
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-02-25 13:21:14 +0100

    añadidas clases student 1

---
+++ src/main/java/acme/entities/leg/Leg.java
@@ -0,0 +1,74 @@
+
+package acme.entities.leg;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import acme.entities.airport.Airport;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Leg extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@ValidString(pattern = "^[A-Z]{3}\\d{4}$")
+	@Mandatory
+	private String				flightNumber;
+
+	@Mandatory
+	@ValidMoment(past = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment(past = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledArrival;
+
+	@Mandatory
+	@ValidScore
+	private Double				durationHours;
+
+	@Mandatory
+	@Valid
+	private LegStatus			status;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Airport				departureAirport;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Airport				arrivalAirport;
+
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne
+	 * private Aircraft aircraft;
+	 */
+}
