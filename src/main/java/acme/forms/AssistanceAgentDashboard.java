f07cbb9edc3d2db4b9e017528e82ea2b0c708cf8
felpennun <felpennun@alum.us.es>
2025-03-11 05:18:54 +0100

    [feat] Create form AssistanceAgentDashboard

---
+++ src/main/java/acme/forms/AssistanceAgentDashboard.java
@@ -0,0 +1,36 @@
+
+package acme.forms;
+
+import java.util.List;
+
+import acme.client.components.basis.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssistanceAgentDashboard extends AbstractForm {
+
+	// Serialisation Identifier
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Ratios de reclamaciones
+	double						ratioResolvedClaims;
+	double						ratioRejectedClaims;
+
+	private List<String>		topThreeMonthsWithMostClaims;
+
+	double						averageClaimLogs;
+	double						deviationClaimLogs;
+	int							minClaimLogs;
+	int							maxClaimLogs;
+
+	double						averageClaimsAssisted;
+	double						deviationClaimsAssisted;
+	int							minClaimsAssisted;
+	int							maxClaimsAssisted;
+
+}
