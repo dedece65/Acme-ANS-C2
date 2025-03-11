616ff55f62a2b4cb1f9e2a5a9b092843c6d1f2d1
angelalopol03 <Angela Lpez>
2025-03-11 16:46:13 +0100

    Entity Review

--- src/main/java/acme/entities/review/Review.java
+++ src/main/java/acme/entities/review/Review.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -58,6 +59,7 @@ public class Review extends AbstractEntity {
 	private Double				score;
 
 	@Mandatory
+	@Valid
 	@Automapped
 	private Boolean				recommended;
 
