e63f34d4419072ba5009fb87e7ddf7e79b8c30d3
angelalopol03 <Angela Lpez>
2025-03-11 16:45:03 +0100

    Entity Review

---
+++ src/main/java/acme/entities/review/Review.java
@@ -0,0 +1,87 @@
+
+package acme.entities.review;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.airline.Airline;
+import acme.entities.airport.Airport;
+import acme.entities.flight.Flight;
+import acme.entities.service.Service;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review extends AbstractEntity {
+
+	// Serialisation version ------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
+	private Date				postedMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidNumber(min = 0.0, max = 10.0)
+	@Automapped
+	private Double				score;
+
+	@Mandatory
+	@Automapped
+	private Boolean				recommended;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@ManyToOne
+	@Optional
+	@Automapped
+	private Airport				airport;
+
+	@ManyToOne
+	@Optional
+	@Automapped
+	private Airline				airline;
+
+	@ManyToOne
+	@Optional
+	@Automapped
+	private Flight				flight;
+
+	@ManyToOne
+	@Optional
+	@Automapped
+	private Service				service;
+}
