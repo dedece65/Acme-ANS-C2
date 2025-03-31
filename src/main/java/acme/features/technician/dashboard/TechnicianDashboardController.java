28938617fc4bb37563813a06ece76cdd8c507926
angelalopol03 <Angela Lpez>
2025-03-31 18:53:02 +0200

    feat: Technician Dashboard (No finish)

---
+++ src/main/java/acme/features/technician/dashboard/TechnicianDashboardController.java
@@ -0,0 +1,12 @@
+
+package acme.features.technician.dashboard;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.forms.TechnicianDashboard;
+import acme.realms.Technician;
+
+@GuiController
+public class TechnicianDashboardController extends AbstractGuiController<Technician, TechnicianDashboard> {
+
+}
---
+++ src/main/java/acme/features/technician/dashboard/TechnicianDashboardShowService.java
@@ -0,0 +1,12 @@
+
+package acme.features.technician.dashboard;
+
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.forms.TechnicianDashboard;
+import acme.realms.Technician;
+
+@GuiService
+public class TechnicianDashboardShowService extends AbstractGuiService<Technician, TechnicianDashboard> {
+
+}
