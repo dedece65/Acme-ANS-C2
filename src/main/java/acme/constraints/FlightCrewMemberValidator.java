e0a5af1a5e23b28977a875067f0e18d38ae0627c
dedece65 <danicastillo2003@gmail.com>
2025-04-02 18:30:45 +0200

    fix/ corrected mistakes in crewmember validation

---
+++ src/main/java/acme/constraints/FlightCrewMemberValidator.java
@@ -0,0 +1,59 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.client.helpers.SpringHelper;
+import acme.client.helpers.StringHelper;
+import acme.realms.FlightCrewMember;
+import acme.realms.FlightCrewMemberRepository;
+
+@Validator
+public class FlightCrewMemberValidator extends AbstractValidator<ValidFlightCrewMember, FlightCrewMember> {
+
+	@Override
+	protected void initialise(final ValidFlightCrewMember annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final FlightCrewMember crewMember, final ConstraintValidatorContext context) {
+
+		assert context != null;
+
+		boolean result;
+
+		if (crewMember == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+			{
+				if (StringHelper.isBlank(crewMember.getCode()))
+					super.state(context, false, "identifier", "java.validation.crewMember.identifier.identifier-couldnt-be-blank");
+				else {
+					String initials = "";
+					String name = crewMember.getUserAccount().getIdentity().getName();
+					String surname = crewMember.getUserAccount().getIdentity().getSurname();
+
+					initials += name.charAt(0);
+					initials += surname.charAt(0);
+
+					boolean validIdentifier = StringHelper.startsWith(crewMember.getCode(), initials, true);
+
+					super.state(context, validIdentifier, "identifier", "java.validation.crewMember.validIdentifier.invalid-identifier");
+				}
+			}
+			{
+				FlightCrewMemberRepository repository;
+				repository = SpringHelper.getBean(FlightCrewMemberRepository.class);
+				boolean repeatedEmployeeCode = repository.findByEmployeeCode(crewMember.getCode(), crewMember.getId()).isEmpty();
+				super.state(context, repeatedEmployeeCode, "identifier", "java.validation.crewMember.repeatedEmployeeCode.message");
+			}
+		}
+
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+}
